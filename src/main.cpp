8804a6f34d13abc65fb6a11774f7a3a519b7faac
Chenzs108 <chenzs108@outlook.com>
2022-11-05 13:12:13 +0000

    Feat: Use the `foo` library in the main program

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
+#include "foo/foo.h"
+
 #include <cstdlib>
+#include <iostream>
 
 
 int main() {
+    std::cout << "The maximum of 2 and 3 is " << Max(2, 3) << std::endl;
     return EXIT_SUCCESS;
 }
